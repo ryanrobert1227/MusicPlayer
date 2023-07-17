92910d39b3e7bf9604f42d28b19fe17307d92c65
GoldFNX <ryanrobert2504@gmail.com>
2023-07-17 18:22:58 -0300

    six try

--- src/music.js
+++ src/music.js
@@ -1,3 +1,15 @@
+// musics
+
+import alone from "./assets/musics/Alone.mp3";
+import anotherLove from "./assets/musics/Another love.mp3";
+import believer from "./assets/musics/Believer.mp3";
+
+// cartazes
+
+import aloneImage from "./assets/Capas/Alone.png";
+import anotherLoveImage from "./assets/Capas/AnotherLove.jpg";
+import believerImage from "./assets/Capas/Believer.jpg";
+
 const data = [
   {
     id: 1,
@@ -42,6 +54,309 @@ const data = [
     lyrics: null,
     cover:
       "https://storage.googleapis.com/pedagogico/frontend-files/aula-react-referencias-eventos/img5.jpg",
+  },
+  {
+    id: 5,
+    title: "Alone",
+    artist: "Alan Walker",
+    description:
+      "Alone is a popular electronic dance music track by Alan Walker. It was released in 2016 and has gained millions of views and streams on various platforms.",
+    url: alone,
+    lyrics: [
+      {
+        verse: [
+          "Lost in your mind, I wanna know",
+          "Am I losin' my mind?",
+          "Never let me go",
+        ],
+      },
+      {
+        verse: [
+          "If this night is not forever",
+          "At least we are together",
+          "I know I'm not alone",
+          "I know I'm not alone",
+        ],
+      },
+      {
+        verse: [
+          "Anywhere, whenever",
+          "Apart but still together",
+          "I know I'm not alone",
+          "I know I'm not alone",
+        ],
+      },
+      {
+        verse: ["I know I'm not alone"],
+      },
+      {
+        verse: ["I know I'm not alone"],
+      },
+      {
+        verse: [
+          "Unconscious mind",
+          "I'm wide awake",
+          "Wanna feel one last time",
+          "Take my pain away",
+        ],
+      },
+      {
+        verse: [
+          "If this night is not forever",
+          "At least we are together",
+          "I know I'm not alone",
+          "I know I'm not alone",
+        ],
+      },
+      {
+        verse: [
+          "Anywhere, whenever",
+          "Apart but still together",
+          "I know I'm not alone",
+          "I know I'm not alone",
+        ],
+      },
+      {
+        verse: ["I know I'm not alone"],
+      },
+      {
+        verse: ["I know I'm not alone"],
+      },
+      {
+        verse: [
+          "I'm not alone",
+          "I'm not alone",
+          "I'm not alone (I know I'm not alone)",
+          "I'm not alone",
+          "I'm not alone",
+          "I'm not alone (I know I'm not alone)",
+        ],
+      },
+    ],
+    cover: aloneImage,
+  },
+  {
+    id: 6,
+    title: "Another Love ",
+    artist: "Tom Odell",
+    description:
+      "Another Love is a beautiful and heartfelt ballad by Tom Odell. It was released in 2012 and received critical acclaim for its emotional lyrics and soulful vocals.",
+    url: anotherLove,
+    lyrics: [
+      {
+        verse: [
+          "I wanna take you somewhere so you know I care",
+          "But it's so cold and I don't know where",
+          "I brought you daffodils in a pretty string",
+          "But they won't flower like they did last spring",
+        ],
+      },
+      {
+        verse: [
+          "And I wanna kiss you, make you feel alright",
+          "I'm just so tired to share my nights",
+          "I wanna cry and I wanna love",
+          "But all my tears have been used up",
+        ],
+      },
+      {
+        verse: [
+          "On another love, another love",
+          "All my tears have been used up",
+          "On another love, another love",
+          "All my tears have been used up",
+          "On another love, another love",
+          "All my tears have been used up",
+          "Oh, oh",
+        ],
+      },
+      { verse: ["Oh, oh, oh, oh, oh", "Oh, oh, oh, oh, oh"] },
+      {
+        verse: [
+          "And if somebody hurts you, I wanna fight",
+          "But my hand's been broken one too many times",
+          "So I'll use my voice, I'll be so fucking rude",
+          "Words, they always win, but I know I'll lose",
+        ],
+      },
+      {
+        verse: [
+          "And I'd sing a song that'd be just ours",
+          "But I sang 'em all to another heart",
+          "And I wanna cry, I wanna learn to love",
+          "But all my tears have been used up",
+        ],
+      },
+      {
+        verse: [
+          "On another love, another love",
+          "All my tears have been used up",
+          "On another love, another love",
+          "All my tears have been used up",
+          "On another love, another love",
+          "All my tears have been used up",
+          "Oh, oh",
+        ],
+      },
+      {
+        verse: [
+          "Ooh, ooh, ooh, ooh, ooh",
+          "Ooh, ooh, ooh, ooh, ooh (oh, need a love now, my heart is thinking of)",
+        ],
+      },
+      {
+        verse: [
+          "I wanna sing a song that'd be just ours",
+          "But I sang 'em all to another heart",
+          "And I wanna cry, I wanna fall in love",
+          "But all my tears have been used up",
+        ],
+      },
+      {
+        verse: [
+          "On another love, another love",
+          "All my tears have been used up",
+          "On another love, another love",
+          "All my tears have been used up",
+          "On another love, another love",
+          "All my tears have been used up",
+        ],
+      },
+      { verse: ["Oh, oh, oh"] },
+    ],
+    cover: anotherLoveImage,
+  },
+  {
+    id: 7,
+    title: "Believer",
+    artist: "Imagine Dragons",
+    description:
+      "Believer is an energetic rock anthem by Imagine Dragons. It was released in 2017 and became a massive hit, known for its powerful vocals and catchy chorus.",
+    url: believer,
+    lyrics: [
+      {
+        verse: [
+          "First things first",
+          "I'ma say all the words inside my head",
+          "I'm fired up and tired of the way that things have been, oh-ooh",
+          "The way that things have been, oh-ooh",
+        ],
+      },
+      {
+        verse: [
+          "Second thing second",
+          "Don't you tell me what you think that I could be",
+          "I'm the one at the sail, I'm the master of my sea, oh-ooh",
+          "The master of my sea, oh-ooh",
+        ],
+      },
+      {
+        verse: [
+          "I was broken from a young age",
+          "Taking my sulking to the masses",
+          "Writing my poems for the few",
+          "That look at me, took to me, shook to me, feeling me",
+          "Singing from heartache from the pain",
+          "Taking my message from the veins",
+          "Speaking my lesson from the brain",
+          "Seeing the beauty through the...",
+        ],
+      },
+      {
+        verse: [
+          "Pain!",
+          "You made me a, you made me a believer, believer",
+          "Pain!",
+          "You break me down and build me up, believer, believer",
+          "Pain!",
+          "Oh, let the bullets fly, oh, let them rain",
+          "My life, my love, my drive, it came from...",
+          "Pain!",
+          "You made me a, you made me a believer, believer",
+        ],
+      },
+      {
+        verse: [
+          "First things first",
+          "Can you imagine what's about to happen?",
+          "It's Weezy the Dragon, I link with the Dragons",
+          "And we gon' get ratchet, no need for imaginin'",
+        ],
+      },
+      {
+        verse: [
+          "This is what's happenin'",
+          "Second thing second, I reckon immaculate",
+          "Sound about accurate",
+          "I know that strength, it don't come, don't come without strategy",
+          "I know the sweet, it don't come without cavities",
+          "I know the passages come with some traffic",
+          "I start with from the basement, end up in the attic",
+          "And third thing third",
+          "Whoever call me out, they simply can't count",
+          "Let's get mathematic, I'm up in this, huh",
+        ],
+      },
+      {
+        verse: [
+          "Is you a believer?",
+          "I get a unicorn out of a zebra",
+          "I wear my uniform like a tuxedo",
+          "This dragon don't hold his breath, don't need no breather",
+          "Love you Ms. Cita, the son of a leader",
+          "I know the bloomin' don't come without rain",
+          "I know the losin' don't come without shame",
+          "I know the beauty don't come without hurt",
+        ],
+      },
+      {
+        verse: [
+          "Hol' up, hol' up, last thing last",
+          "I know that Tunechi don't come without Wayne",
+          "I know that losin' don't come without game",
+          "I know that glory don't come without...",
+          "Don't come without...",
+        ],
+      },
+      {
+        verse: [
+          "Pain!",
+          "You made me a, you made me a believer, believer",
+          "Pain!",
+          "You break me down and build me up, believer, believer",
+          "Pain",
+          "Oh, let the bullets fly, oh, let them rain",
+          "My life, my love, my drive, it came from...",
+          "Pain!",
+          "You made me a, you made me a believer, believer",
+        ],
+      },
+      {
+        verse: [
+          "Last things last",
+          "By the grace of fire and flames",
+          "You're the face of the future, the blood in my veins, oh-ooh",
+          "The blood in my veins, oh-ooh",
+          "But they never did, ever lived, ebbing and flowing",
+          "Inhibited, limited 'til it broke open and rained down",
+          "It rained down, like...",
+        ],
+      },
+      {
+        verse: [
+          "Pain!",
+          "You made me a, you made me a believer, believer",
+          "Pain!",
+          "You break me down and build me up, believer, believer",
+          "Pain",
+          "Oh, let the bullets fly, oh, let them rain",
+          "My life, my love, my drive, it came from...",
+          "Pain!",
+          "You made me a, you made me a believer, believer",
+        ],
+      },
+    ],
+    cover: believerImage,
   },
 ];
 
